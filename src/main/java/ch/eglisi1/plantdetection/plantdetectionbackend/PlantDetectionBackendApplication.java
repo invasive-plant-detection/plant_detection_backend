04ad7011007d312c5baab18e4337d7c20a69a1ec
Simon Egli <eglisi1@students.zhaw.ch>
2024-01-15 09:30:26 +0100

    add sprint boot initializr content

---
+++ src/main/java/ch/eglisi1/plantdetection/plantdetectionbackend/PlantDetectionBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.eglisi1.plantdetection.plantdetectionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantDetectionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantDetectionBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/eglisi1/plantdetection/plantdetectionbackend/PlantDetectionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.eglisi1.plantdetection.plantdetectionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantDetectionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
