0efdd2d1f2c04069247dfee977e0a6387ece58f0
Simon Egli <eglisi1@students.zhaw.ch>
2024-04-22 15:06:34 +0200

    use specific spring version in pom

--- src/test/java/ch/eglisi1/plantdetection/plantdetectionbackend/controller/PredictionControllerTest.java
+++ src/test/java/ch/eglisi1/plantdetection/plantdetectionbackend/controller/PredictionControllerTest.java
@@ -12,12 +12,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
 class PredictionControllerTest {
 
     private MockMvc mockMvc;
--- src/test/java/ch/eglisi1/plantdetection/plantdetectionbackend/service/PredictionServiceTest.java
+++ src/test/java/ch/eglisi1/plantdetection/plantdetectionbackend/service/PredictionServiceTest.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.ResourceAccessException;
 
 @SpringBootTest
-//@TestPropertySource("classpath:application.properties")
+@ActiveProfiles("test")
 class PredictionServiceTest {
     @Autowired
     PredictionService predictionService;
