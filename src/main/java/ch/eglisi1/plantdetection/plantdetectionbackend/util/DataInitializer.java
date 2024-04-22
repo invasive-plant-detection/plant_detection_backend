314e0ff62d8d017b002f06ccd7087e60c5f64428
Simon Egli <eglisi1@students.zhaw.ch>
2024-04-22 14:53:08 +0200

    add profile for deployment

--- src/main/java/ch/eglisi1/plantdetection/plantdetectionbackend/util/DataInitializer.java
+++ src/main/java/ch/eglisi1/plantdetection/plantdetectionbackend/util/DataInitializer.java
@@ -6,6 +6,7 @@ import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
@@ -20,6 +21,9 @@ public class DataInitializer {
     private final Random random = new Random();
     private final PredictionRepository predictionRepository;
 
+    @Value("${environment.name}")
+    private String environmentName;
+
     @Autowired
     public DataInitializer(PredictionRepository predictionRepository) {
         this.predictionRepository = predictionRepository;
@@ -27,18 +31,20 @@ public class DataInitializer {
 
     @PostConstruct
     public void initData() {
-        int entitiesToCreate = 100;
-        String base64Image = "placeholder_for_base64_image";
-        String prediction = "Here will be my prediction that is better than a coin flip";
-        for (int i = 0; i < entitiesToCreate; i++) {
-            BigDecimal latitude = BigDecimal.valueOf(generateRandomCoordinate(46.8, 47.8)); // Approximate range for latitude in German-speaking Switzerland
-            BigDecimal longitude = BigDecimal.valueOf(generateRandomCoordinate(6.9, 10.5)); // Approximate range for longitude in German-speaking Switzerland
+        if ("DEV".equals(environmentName)) {
+            int entitiesToCreate = 100;
+            String base64Image = "placeholder_for_base64_image";
+            String prediction = "Here will be my prediction that is better than a coin flip";
+            for (int i = 0; i < entitiesToCreate; i++) {
+                BigDecimal latitude = BigDecimal.valueOf(generateRandomCoordinate(46.8, 47.8)); // Approximate range for latitude in German-speaking Switzerland
+                BigDecimal longitude = BigDecimal.valueOf(generateRandomCoordinate(6.9, 10.5)); // Approximate range for longitude in German-speaking Switzerland
 
-            PredictionDbo predictionDbo = new PredictionDbo(null, latitude, longitude, randomDateTimeWithinLast100Days(), base64Image, prediction);
-            PredictionDbo savedDbo = predictionRepository.save(predictionDbo);
-            logger.debug("Created Entity:\n{}", savedDbo);
+                PredictionDbo predictionDbo = new PredictionDbo(null, latitude, longitude, randomDateTimeWithinLast100Days(), base64Image, prediction);
+                PredictionDbo savedDbo = predictionRepository.save(predictionDbo);
+                logger.debug("Created Entity:\n{}", savedDbo);
+            }
+            logger.info("Created {} entities", entitiesToCreate);
         }
-        logger.info("Created {} entities", entitiesToCreate);
     }
 
     private double generateRandomCoordinate(double min, double max) {
