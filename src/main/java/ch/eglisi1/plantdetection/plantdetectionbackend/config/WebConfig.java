2d38ae772c47313ea0a6a54d9db7649b341d7d52
Simon Egli <eglisi1@students.zhaw.ch>
2024-04-23 10:08:02 +0200

    add cors for localhost ng and prod ng

--- src/main/java/ch/eglisi1/plantdetection/plantdetectionbackend/config/WebConfig.java
+++ src/main/java/ch/eglisi1/plantdetection/plantdetectionbackend/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "https://plant-detection-frontend-e069a2bba139.herokuapp.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
