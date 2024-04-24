f3b8728d125da2524aa49273e379be196488a528
Simon Egli <eglisi1@students.zhaw.ch>
2024-04-24 10:10:57 +0200

    remove unused method

--- src/main/java/ch/eglisi1/plantdetection/plantdetectionbackend/repository/PredictionRepository.java
+++ src/main/java/ch/eglisi1/plantdetection/plantdetectionbackend/repository/PredictionRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface PredictionRepository extends JpaRepository<PredictionDbo, UUID> {
-    PredictionDbo searchByPredictionId(UUID predictionId);
 }
